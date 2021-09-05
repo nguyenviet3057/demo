8f8d77c9b649ce6a86607de22581b9d109683260
Viet <nguyenviet3057@gmail.com>
2021-09-05 09:59:52 +0700

    DELETE ALL

--- demo.js
+++ demo.js
@@ -12,6 +12,12 @@ var yes_answer = document.querySelector('#yes');
 var background = document.getElementById('background');
 var music = document.getElementById('music');
 
+function on_yes()
+{
+	yes_answer.style.boxShadow = "0px 0px 100px 10px red";
+}
+yes_answer.onmouseover = on_yes;
+
 function change_background()
 {
 	question.style.opacity = "0%";
